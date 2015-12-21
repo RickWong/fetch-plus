97da25e0cb7b85144ccd60de056488da878ec96a
Rick <wong@mollie.com>
2015-12-22 00:00:38 +0100

    Now fetch-plus-json automatically stringifies plain objects to JSON.

--- packages/fetch-plus-json/src/index.js
+++ packages/fetch-plus-json/src/index.js
@@ -5,6 +5,10 @@
 export const before = (request) => {
 	request.options.headers["Accept"]       = "application/json";
 	request.options.headers["Content-Type"] = "application/json; charset=utf-8";
+
+	if (typeof request.options.body === "object") {
+		request.options.body = JSON.stringify(request.options.body);
+	}
 };
 
 export const after = (response) => {
