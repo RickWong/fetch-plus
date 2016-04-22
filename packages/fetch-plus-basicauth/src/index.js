0f2f4e969b44613a97866ba69af02aebd4c238a0
Baptiste JAMIN <baptistejamin@gmail.com>
2016-04-22 19:06:43 +0200

    Fix third parameter on fetch plus basic auth

--- packages/fetch-plus-basicauth/src/index.js
+++ packages/fetch-plus-basicauth/src/index.js
@@ -5,9 +5,11 @@ import {compute} from "utils/compute";
 
 // Export using middleware creation notation.
 module.exports = (username, password, _btoa = null) => (request) => {
+	
 	if (typeof btoa === "undefined" && !_btoa) {
 		throw new TypeError("btoa() function required but not available");
 	}
-
-	request.options.headers["Authorization"] = "Basic " + btoa(compute(username) + ":" + compute(password));
+	else
+		_btoa = btoa;
+	request.options.headers["Authorization"] = "Basic " + _btoa(compute(username) + ":" + compute(password));
 };
