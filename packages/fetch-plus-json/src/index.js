89f1dfe0ac99e54f234feb952354b26eaa879c56
Rick <wong@mollie.com>
2016-04-29 22:20:42 +0200

    Don't export before and after. Handle case where json() does not return a Promise.

--- packages/fetch-plus-json/src/index.js
+++ packages/fetch-plus-json/src/index.js
@@ -2,45 +2,48 @@
  * @copyright © 2015, Rick Wong. All rights reserved.
  */
 
-export const before = (request) => {
+const after = (options = {}) => (response) => {
+	const resolveWith = (value) => {
+		if (!options.fullResponse) {
+			return value;
+		}
+
+		// When requested to return the full response, overwrite
+		// the body property
+		Object.defineProperty(response, "body", {
+			get: () => value
+		});
+
+		return response;
+	};
+
+	let json = response.json();
+
+	// The return value of json() is not always Promise (?)
+	// Issue: https://github.com/bitinn/node-fetch/issues/111
+	if (typeof json !== "object" || typeof json.then !== "function") {
+		json = Promise.resolve(json);
+	}
+
+	return json.then(resolveWith).catch((error) => {
+		// JSON parse failed
+		// If status is 204, assume there was no data
+		if (response.status === 204) {
+			return resolveWith(null);
+		}
+
+		throw error;
+	});
+};
+
+module.exports = (options = {}) => (request) => {
 	request.options.headers["Accept"]       = "application/json";
 	request.options.headers["Content-Type"] = "application/json; charset=utf-8";
 
 	if (typeof request.options.body === "object") {
 		request.options.body = JSON.stringify(request.options.body);
 	}
-};
 
-export const after = (options = {}) => (response) => {
-	return new Promise((resolve, reject) => {
-		// When requested to return the full response, overwrite
-		// the body property
-		const resolveWith = (value) => {
-			if (options.fullResponse) {
-				Object.defineProperty(response, "body", {
-					get: () => value
-				});
-
-				resolve(response);
-			} else {
-				resolve(value);
-			}
-		};
-
-		response.json().then(resolveWith).catch((err) => {
-			// JSON parse failed
-			// If status is 204, assume there was no data
-			if (response.status === 204) {
-				resolveWith(null);
-			} else {
-				reject(err);
-			}
-		});
-	});
-};
-
-module.exports = (options = {}) => (request) => {
-	before(request);
 	return after(options);
 };
 
